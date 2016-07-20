e96fd4755b79b4e04c8e46e289a01dc5fa4cd327
JenPhD <jpemberton76@gmail.com>
2016-07-20 14:08:07 -0400

    interface

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,46 @@
-$( document ).ready(function(){
+$(document).ready(function() {
 
-	//click event to pick a theorist
-	var theorist = new Array(4);
-	theorist[0] = new Theorist ('Horkheimer', 'horkheimer.jpg', 120, 16, 23);
-	theorist[1] = new Theorist ('Adorno', 'adorno.jpg', 130, 18, 24);
-	theorist[2] = new Theorist ('Marcuse', 'marcuse.jpg', 140, 20, 35);
-	theorist[3] = new Theorist ('Habermas', 'habermas.jpg', 150, 22, 59);
+//fade in header
+$(':header').hide().fadeIn(1500);
+$('#action').hide().fadeIn(1500);
+
+	var theorists = new Array(4);
+	theorists[0] = new Theorist ('Horkheimer', 'horkheimer.jpg', 120, 16, 23);
+	theorists[1] = new Theorist ('Adorno', 'adorno.jpg', 130, 18, 24);
+	theorists[2] = new Theorist ('Marcuse', 'marcuse.jpg', 140, 20, 35);
+	theorists[3] = new Theorist ('Habermas', 'habermas.jpg', 150, 22, 59);
+
+console.log(theorists);
 
 	var hitpoints = [120, 130, 140, 150];
 
 	var player = -1;
 	var opponent = -1;
-	var defeatedArray = new Array;  // Array of defeated opponents
+	var defeatedRow = new Array;  // Array of defeated opponents
 	var roundOver = false;
 	var numWins = 0;
 
 	function Theorist(name, image, health, attack, counter) {
-		this.name = 'name';
-		this.image = 'image';
-		this.health = 'health';
-		this.attack = 'attack';
-		this.counter = 'counter';
+		this.name = name;
+		this.image = image;
+		this.health = health;
+		this.attack = attack;
+		this.counter = counter;
 		this.status = 'available';
 	}
 
 	function showTheoristPool() {
-		//$('#theorist').empty();
-		var ctr;
-		for (ctr = 0; ctr < theorist.length; ctr++) {
+		$('#theorists').empty();
+		for (ctr = 0; ctr < theorists.length; ctr++) {
 			// Check to see if they have been selected to play first
 			// Will also need to check if they have been eliminated
-			if (theorist[ctr].status == 'opponent') {
+			if (theorists[ctr].status == 'available') {
 				var $newTheorist = $('<div>')
-					//.addClass('theorist col-sm-3')
+					.addClass('theorist col-sm-3')
 					.attr('theorist-id', ctr)
-					.html('<span class="name">'+ theorist[ctr].name + '</span><img src="../assets/images/'+ theorist[ctr].image +'"><span class="points">('+ theorist[ctr].health + ')</span>');
+					.html('<span class="name">'+ theorists[ctr].name + '</span><img src="assets/images/'+ theorists[ctr].image +'"><span class="points">('+ theorists[ctr].health + ')</span>');
 					// $newTheorist.on('click', selectTheorist(ctr));
-				$('#theorist').append($newTheorist);				
+				$('#theorists').append($newTheorist);				
 			}
 		}
 		$('.theorist').on('click', function() {
@@ -47,63 +51,66 @@ $( document ).ready(function(){
 	function showPlayer(index) {
 		var $newPlayer = $('<div>')
 			.addClass(' player col-sm-offset-1 col-sm-3 ')
-			.html('<span class="name">'+ theorist[index].name + ' (' + theorist[index].health + ')</span><img src="../assets/images/'+ theorist[index].image +'"><span class="points">'+ 'Attack ' + theorist[index].attack + ' | Counter ' +  + theorist[index].counter +'</span>');
+			.html('<span class="name">'+ theorists[index].name + ' (' + theorists[index].health + ')</span><img src="assets/images/'+ theorists[index].image +'"><span class="points">'+ 'Attack ' + theorists[index].attack + ' | Counter ' +  + theorists[index].counter +'</span>');
 		$('#player').html($newPlayer);
 		$('#player-header').html('Player');
-		$('#versus').html('<img src="..assets/images/vs.png">');
+		$('#versus').html('<img src="assets/images/vs3.png">');
 	}
 
 	function showOpponent(index) {
 		var $newOpponent = $('<div>')
 			.addClass('opponent col-sm-offset-1 col-sm-3 ')
-			.html('<span class="name">'+ theorist[index].name + ' (' + theorist[index].health + ')</span><img src="../assets/images/'+ theorist[index].image +'"><span class="points">'+ 'Attack ' + theorist[index].attack + ' | Counter ' +  + theorist[index].counter +'</span>');
+			.html('<span class="name">'+ theorists[index].name + ' (' + theorists[index].health + ')</span><img src="assets/images/'+ theorists[index].image +'"><span class="points">'+ 'Attack ' + theorists[index].attack + ' | Counter ' +  + theorists[index].counter +'</span>');
 		$('#opponent').html($newOpponent);
 		$('#opponent-header').html('Opponent');
-		$('#versus').html('<img src="../assets/images/#.png"><h3>Communicative Action!<h3>');
+		$('#versus').html('<img src="assets/images/debate.jpg"><h3>Go!<h3>');
 	}
-	function battle() {
-		theorist[opponent].health = theorist[opponent].health	- theorist[player].attack;
-		theorist[player].health = theorist[player].health	- theorist[opponent].counter;		
-		if (theorist[opponent].health < 1) {
+
+	function debate() {
+		theorists[opponent].health = theorists[opponent].health	- theorists[player].attack;
+		theorists[player].health = theorists[player].health	- theorists[opponent].counter;		
+		if (theorists[opponent].health < 1) {
 			// Round over, player wins
-			theorist[opponent].health = 0;
-			theorist[opponent].status = 'lost';
+			theorists[opponent].health = 0;
+			theorists[opponent].status = 'lost';
 			nextRound();
-		} else if (theorist[player].health < 1) {
+		} else if (theorists[player].health < 1) {
 			// Round over, opponent wins
-			theorist[player].health = 0;
+			theorists[player].health = 0;
 			gameOver();
 		}
-		theorist[player].attack = theorist[player].attack + 5;
+		theorists[player].attack = theorists[player].attack + 5;
 		refreshDisplay();
 	}
 
 	function nextRound() {
-		defeatedArray.push(opponent);
+		defeatedRow.push(opponent);
 		numWins++;
 		if (numWins > 2) {
 			playerWins();
 		}
 		opponent = -1;
-		theorist[player].health = hitpoints[player];
+		theorists[player].health = hitpoints[player];
 		$('#opponent').empty();		
 		refreshDisplay();
 		$('#lost').empty();
-		showDefeatedArray();		
+		showDefeatedRow();		
 	}
+
 	function gameOver() {
-		$('#battle').html("<h2>Game Over!<h2>");
+		$('#debate').html("<h2>Game Over<h2>");
 	}
+
 	function playerWins() {
-		$('#battle').html('<h2>You win! Now go read something!<span class="name">'+ theorist[player].name + '</span><img src="assets/images/'+ theorist[player].image +'"><span class="points"> </span></h2>');
+		$('#debate').html('<h2>Winner, winner, chicken dinner!!<span class="name">'+ theorists[player].name + '</span><img src="assets/images/'+ theorists[player].image +'"><span class="points"> </span></h2>');
 	}
 	function refreshDisplay () {
 		showTheoristPool();
 		if (player != -1) {
 			showPlayer(player);
-			if (defeatedArray.length > 1) {
-				$('#action').html("Final battle");			
-			} else if (defeatedArray.length > 0) {
+			if (defeatedRow.length > 1) {
+				$('#action').html("Final debate");			
+			} else if (defeatedRow.length > 0) {
 				$('#action').html("Select your next opponent");			
 			} else {
 				$('#action').html("Select your opponent");			
@@ -111,19 +118,19 @@ $( document ).ready(function(){
 		}
 		if (opponent != -1) {
 			showOpponent(opponent);
-			if (defeatedArray.length < 2) {
-				// Don't show this for the final battle
-				$('#action').html("Start your engines!");	
+			if (defeatedRow.length < 2) {
+				// Don't show this for the final race
+				$('#action').html("One minute debate warning!");	
 			}
 		}
 	}
-	function showDefeatedArray() {
-		for (ctr = 0; ctr < theorist.length; ctr++) {
+	function showDefeatedRow() {
+		for (ctr = 0; ctr < theorists.length; ctr++) {
 			$('#lost').empty;
-				if (theorist[ctr].status == 'lost') {
+				if (theorists[ctr].status == 'lost') {
 					var $lostTheorist = $('<div>')
 						.addClass('lostTheorist col-sm-3 ')
-						.html('<span class="name">'+ theorist[ctr].name + '</span><img src="assets/images/'+ theorist[ctr].image +'"><span> </span>');
+						.html('<span class="name">'+ theorists[ctr].name + '</span><img src="assets/images/'+ theorists[ctr].image +'"><span> </span>');
 					$('#lost').append($lostTheorist);	
 				}
 		}
@@ -132,18 +139,18 @@ $( document ).ready(function(){
 		if (player === -1 && opponent === -1 ) {
 			// Nothing selected so this is the player
 			player = index;
-			theorist[player].status = 'player';
+			theorists[player].status = 'player';
 		} else if (opponent === -1) {
 			// player already picked, so pick the opponent
 			opponent = index;
-			theorist[opponent].status = 'opponent';			
+			theorists[opponent].status = 'opponent';			
 		}
 		refreshDisplay();
 	}
 
 	$('#versus').on('click', function () {
 		if (player > -1 && opponent > -1) {
-			battle();
+			debate();
 		}
 	});
 
